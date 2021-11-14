10ef713d4aa530065f69cb139a0e5c2cdd43c1c0
Daniel <KunitskiiDanik@mail.ru>
2021-11-14 23:47:46 +0300

    added lesson_6

--- lesson_6(easy and hard)/script.js
+++ lesson_6(easy and hard)/script.js
@@ -6,18 +6,25 @@ let count = 10;
 function checkNumber(num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
+let numb;
+let correctNumber = function() {
 
-let correctNumber = function(numb) {
     numb = prompt('Введите число.');
     if (numb === null) {
         alert('Игра окончена.');
         return numb;
-    } else if (!checkNumber(numb)) correctNumber(numb);
-    else if (numb > 100 || numb <= 0) falseNumber(numb);
+    } else if (!checkNumber(numb)) {
+        numb = null;
+        correctNumber();
+    } else if (numb > 100 || numb <= 0) {
+        falseNumber();
+    }
+
+    return numb;
 }
-let falseNumber = function(num) {
+let falseNumber = function() {
     alert('Error: Введено число в неправильном диапазоне. Повторите попытку..');
-    correctNumber(num);
+    correctNumber();
 }
 
 let countAttempts = function() {
@@ -52,6 +59,7 @@ let comprasionRandom = function(answer, randomNumber) {
     if (answer == randomNumber) {
         let complete = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
         complete ? (
+            count = 10,
             randomNum(),
             answerInput()
         ) : (
@@ -71,14 +79,16 @@ let randomNum = function() {
 }
 
 let answerInput = function() {
-    let invalid;
-    let answerUser = prompt('Угадай число от 1 до 100:');
-    if (answerUser === null) return alert('Игра окончена.');
 
+    let answerUser = prompt('Угадай число от 1 до 100:');
+
+    if (answerUser === null) return alert('Игра окончена.');
     if (!checkNumber(answerUser)) {
-        invalid = correctNumber(answerUser);
-    } else if (answerUser > 100 || answerUser <= 0) falseNumber(answerUser);
-    if (invalid !== null) comprasionRandom(answerUser, random);
+        answerUser = correctNumber();
+    } else if (answerUser > 100 && answerUser <= 0) falseNumber(answerUser);
+
+    if (answerUser) comprasionRandom(answerUser, random);
+
 }
 
 randomNum();
