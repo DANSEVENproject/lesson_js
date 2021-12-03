1219b12dc84c33383caef21a33f2b9a517e492f7
Daniel <KunitskiiDanik@mail.ru>
2021-12-03 12:59:19 +0300

    add bonus_2

---
+++ bonus_2/main.js
@@ -0,0 +1,21 @@
+const myLesson = [
+    { lesson: 1, status: true },
+    { lesson: 2, status: true },
+    { lesson: 3, status: false },
+    { lesson: 4, status: true },
+    { lesson: 5, status: false },
+    { lesson: 6, status: true },
+    { lesson: 7, status: true },
+    { lesson: 8, status: true },
+    { lesson: 9, status: true },
+    { lesson: 10, status: false }
+];
+
+const falseLesson = myLesson.filter((item) => (item.status === false));
+
+
+console.log(falseLesson)
+    /*
+    Доп Задание:
+    Необходимо вывести в консоль массив с заданиями у которых статус false
+    */
